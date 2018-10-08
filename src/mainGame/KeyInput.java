8149d72d3c2f340a5a646993e840dd1858cb7528
pnam99 <43445638+pnam99@users.noreply.github.com>
2018-10-08 12:36:09 -0400

    Arrow Keys and Escape Key added

--- src/mainGame/KeyInput.java
+++ src/mainGame/KeyInput.java
@@ -3,6 +3,8 @@ package mainGame;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+import javax.swing.JOptionPane;
+
 import mainGame.Game.STATE;
 
 /**
@@ -23,7 +25,8 @@ public class KeyInput extends KeyAdapter {
 	private Spawn1to10 spawner;
 	private Upgrades upgrades;
 	private String ability;
-
+	private int escCount; // Experimental Code with the Escape Key
+	
 	// uses current handler created in Game as parameter
 	public KeyInput(Handler handler, Game game, HUD hud, Player player, Spawn1to10 spawner, Upgrades upgrades) {
 		this.handler = handler;
@@ -33,6 +36,7 @@ public class KeyInput extends KeyAdapter {
 		this.hud = hud;
 		this.spawner = spawner;
 		this.upgrades = upgrades;
+		escCount = 0; // Experimental Code with the Escape Key
 		keyDown[0] = false;
 		keyDown[1] = false;
 		keyDown[2] = false;
@@ -52,19 +56,19 @@ public class KeyInput extends KeyAdapter {
 			// using only if's allows multiple keys to be triggered at once
 			if (tempObject.getId() == ID.Player) {// find the player object, as he is the only one the user can control
 				// key events for player 1
-				if (key == KeyEvent.VK_W) {
+				if (key == KeyEvent.VK_W || key == KeyEvent.VK_UP) {
 					tempObject.setVelY(-(this.speed));
-					keyDown[0] = true;
+					keyDown[0] = true;					
 				}
-				if (key == KeyEvent.VK_A) {
+				if (key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT) {
 					tempObject.setVelX(-(this.speed));
 					keyDown[1] = true;
 				}
-				if (key == KeyEvent.VK_S) {
+				if (key == KeyEvent.VK_S || key == KeyEvent.VK_DOWN) {
 					tempObject.setVelY(this.speed);
 					keyDown[2] = true;
 				}
-				if (key == KeyEvent.VK_D) {
+				if (key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT) {
 					tempObject.setVelX(this.speed);
 					keyDown[3] = true;
 				}
@@ -96,13 +100,13 @@ public class KeyInput extends KeyAdapter {
 
 			if (tempObject.getId() == ID.Player) {
 				// key events for player 1
-				if (key == KeyEvent.VK_W)
+				if (key == KeyEvent.VK_W || key == KeyEvent.VK_UP)
 					keyDown[0] = false;// tempObject.setVelY(0);
-				if (key == KeyEvent.VK_A)
+				if (key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT)
 					keyDown[1] = false;// tempObject.setVelX(0);
-				if (key == KeyEvent.VK_S)
+				if (key == KeyEvent.VK_S || key == KeyEvent.VK_DOWN)
 					keyDown[2] = false;// tempObject.setVelY(0);
-				if (key == KeyEvent.VK_D) {
+				if (key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT) {
 					keyDown[3] = false;// tempObject.setVelX(0);
 					keyDown[4] = false;
 				}
@@ -116,8 +120,18 @@ public class KeyInput extends KeyAdapter {
 			}
 
 		}
-
-		// if (key == KeyEvent.VK_ESCAPE) System.exit(1);
+		
+		// Experimental Code with the Escape Key (Game Quit Confirmation)
+		if (key == KeyEvent.VK_ESCAPE) {
+//			game.stop();
+			int dialogButton = JOptionPane.YES_NO_OPTION;
+			JOptionPane.showConfirmDialog(null,"Would you like to quit the game?", "Warning",dialogButton);
+			if(dialogButton==JOptionPane.YES_OPTION) {
+				System.exit(0);
+			}
+		}
+		 
 	}
-
 }
+
+
