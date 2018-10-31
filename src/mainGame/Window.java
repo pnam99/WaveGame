b7f5a8e0fd757c4ba717dd77e050cfef830ca79c
coolmanthegreat <james101544@gmail.com>
2018-10-31 13:09:07 -0400

    <Add Jpanel as border>

--- src/mainGame/Window.java
+++ src/mainGame/Window.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * Loads the window of the game, and sets the proper dimensions
@@ -12,7 +13,7 @@ import javax.swing.JFrame;
  * 5/30/16
  */
 
-public class Window extends Canvas{
+public class Window extends JFrame{
 
 	private static final long serialVersionUID = 1L;
 	
@@ -27,6 +28,9 @@ public class Window extends Canvas{
 		frame.setLocationRelativeTo(null);
 		frame.add(game);
 		frame.setVisible(true);
+		JPanel panel=new JPanel();
+		panel.setBackground(Color.WHITE);
+		frame.add(panel);
 		game.start();
 		
 		
